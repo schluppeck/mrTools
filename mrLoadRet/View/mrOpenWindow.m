schluppeck/mrTools

commit 96df1572205fd38487f487a1a04490dcda864580

Fixing annoying problem with new version of matlab that cannot load variables with figure handles. Now we check for an old version of the mrLastView and do not try to load when we hit one - warning user what to do

mrLoadRet/View/mrOpenWindow.m
@@ -81,7 +81,7 @@ if ~isempty(which('mlrPlugin')), view = mlrPlugin(view);end
 baseLoaded = 0;
 if ~isempty(mrLastView) && isfile(sprintf('%s.mat',stripext(mrLastView)))
   disppercent(-inf,sprintf('(mrOpenWindow) Loading %s',mrLastView));
-  mrLastView=load(mrLastView);
+  mrLastView=loadLastView(mrLastView);
   disppercent(inf);
   % if the old one exists, then set up fields
 %   disppercent(-inf,'(mrOpenWindow) Restoring last view');
@@ -164,7 +164,7 @@ if ~isempty(mrLastView) && isfile(sprintf('%s.mat',stripext(mrLastView)))
       disppercent(inf);
     end
     % check panels that need to be hidden
-    if isfield(mrLastView.viewSettings,'panels')
+    if isfield(mrLastView,'viewSettings') && isfield(mrLastView.viewSettings,'panels')
       for iPanel = 1:length(mrLastView.viewSettings.panels)
 	% if it is not displaying then turn it off
 	if ~mrLastView.viewSettings.panels{iPanel}{4}

mrLoadRet/File/loadLastView.m
@@ -1,0 +1,70 @@
+% loadLastView.m
+%
+%        $Id:$ 
+%      usage: loadLastView()
+%         by: justin gardner
+%       date: 04/02/15
+%    purpose: This function became necessary since in version 8.2, mathworks 
+%             inexplicably busted figure handles. Now they are handles and
+%             if you load an old variable with a handle, it causes massive
+%             convulsions in matlab. It also tries to pop up a figure - which
+%             is not what we want - eeks. Anyway, the solution is to load
+%             part of the mrLastView variable (viewSettings) and check
+%             its version - if it is an old style version then warn the user
+%             that we can't load
+%
+function [v viewSettings]  = loadLastView(filename)
+
+v = [];
+viewSettings = [];
+
+% check arguments
+if ~any(nargin == [0 1])
+  help loadLastView
+  return
+end
+
+if nargin == 0;
+  filename = 'mrLastView.mat';
+end
+filename = setext(filename,'mat');
+
+if ~isfile(filename);
+  mrWarnDlg('(loadLastView) Could not find %s',filename);
+  return
+end
+
+% new version of matlab needs to check if we are trying to load an old file
+if mlrGetMatlabVersion >= 8
+  % load the viewSettings part and look to see if it is a new file
+  check = load(filename,'viewSettings');
+  if isfield(check,'viewSettings') && isfield(check.viewSettings,'version') && (check.viewSettings.version>=2.0)
+    % then we are ok, load the view part
+    l = load(filename,'view');
+    % return them both
+    if nargout == 1
+      % return as single argument
+      v = l;
+    else
+      % or as two
+      v = l.view;
+      viewSettings = check.viewSettings;
+    end
+    return
+  else
+    mrWarnDlg(sprintf('(loadLastView) The mrLastView found: %s is from an older version of matlab which allowed saving figure handles. The geniuses at Mathworks have busted that, so loading this file will no longer work. Moving mrLastView.mat to mrLastView.mat.old You will lose any rois that were loaded but not saved and mrLoadRet will start up without bases and analyses loaded. If you really need what was in the viewer we suggest running on an earlier version of matlab - you just then need to copy mrLastView.mat.old back to mrLastView.mat, open mrLoadRet and then quit - this will save the file back w/out the offending figure handles. Send complaints to Mathworks!',filename),'Yes');
+    movefile(filename,sprintf('%s.old',filename));
+    return
+  end
+else
+  % otherwise just load
+  l = load('mrLastView');
+  if nargout == 1
+    v = l;
+  else
+    v = l.view;
+    viewSettings = l.viewSettings;
+  end
+end
+
+

mrLoadRet/File/saveSform.m
@@ -134,24 +134,29 @@ saveSession
 % also remove any base anatomies from mrLastView if it is
 % there since those might have a different sform
 if isfile('mrLastView.mat')
-  load mrLastView
-  %here do something something stupid with variable view, otherwise the code analyser is not happy
-  %because view is also a function name
-  view.baseVolumes;
-%JB: Any base anatomy that is in canonical anatomical base 
-% shouldn't have to be changed, so no need to remove all of them.
-% better to let the user choose wich bases to remove
-  %view.baseVolumes = [];
-  baselist = selectInList(view,'base','Choose bases to remove',[]);
-  if ~isempty(baselist)
-    disp(sprintf('(saveSform) Removing base anatomies from mrLastView. Note that if  you have a session open, you still have to remove the bases manually.'));
-    view.baseVolumes(baselist) = [];
-    if getfield(whos('view'),'bytes')<2e9
-      eval(sprintf('save %s view viewSettings -V6;','mrLastView'));
-    else
-      mrWarnDlg('(mrQuit) Variable view is more than 2Gb, using option -v7.3 to save');
-      eval(sprintf('save %s view viewSettings -v7.3;','mrLastView'));
+  try
+    [view viewSettings] = loadLastView;
+    if isempty(view),return,end
+    %here do something something stupid with variable view, otherwise the code analyser is not happy
+    %because view is also a function name
+    view.baseVolumes;
+    %JB: Any base anatomy that is in canonical anatomical base 
+    % shouldn't have to be changed, so no need to remove all of them.
+    % better to let the user choose wich bases to remove
+    %view.baseVolumes = [];
+    baselist = selectInList(view,'base','Choose bases to remove',[]);
+    if ~isempty(baselist)
+      disp(sprintf('(saveSform) Removing base anatomies from mrLastView. Note that if  you have a session open, you still have to remove the bases manually.'));
+      view.baseVolumes(baselist) = [];
+      if getfield(whos('view'),'bytes')<2e9
+	eval(sprintf('save %s view viewSettings -V6;','mrLastView'));
+      else
+	mrWarnDlg('(mrQuit) Variable view is more than 2Gb, using option -v7.3 to save');
+	eval(sprintf('save %s view viewSettings -v7.3;','mrLastView'));
+      end
     end
+  catch
+    disp(sprintf('(saveSform) Error loading mrLastView. This is probably due to some Mathworks issues trying to load a mat file that has old-style figure handles in it. You will need to remove the file mrLastView.mat since Mathworks can no longer seem to load it (complain to them ;-)'));
   end
 end
 deleteView(v);

mrLoadRet/GUI/mrSaveView.m
@@ -38,6 +38,11 @@ try
   % replace view.figure with figure number (to prevent opening on loading
   % of the .mat file)
   view.figure = mlrGetFignum(view);
+  % put a setting in viewSettings to tell that we have a good handle
+  % so that we can load in versions of mathworks that have recently
+  % become too stupide to be able to load objects with the old style
+  % figure handles
+  viewSettings.version = 2.0;
   
   if getfield(whos('view'),'bytes')<2e9
     save(fullfile(homeDir,'mrLastView'), 'view','viewSettings', '-V6');

mrUtilities/File/Nifti/mrUpdateNiftiHdr.m
@@ -96,7 +96,7 @@ saveSession;
 % there since those might have a different sform
 if isfile('mrLastView.mat')
   disp(sprintf('(mrUpdateNiftiHdr) Removing base anatomies from mrLastView'));
-  load mrLastView
+  [view viewSettings] = loadLastView;
   view.baseVolumes = [];
   save mrLastView view viewSettings
 end

mrUtilities/MatlabUtilities/mlrGetMatlabVersion.m
@@ -1,0 +1,30 @@
+% mlrGetMatlabVersion.m
+%
+%        $Id:$ 
+%      usage: versionNum = mlrGetMatlabVersion()
+%         by: justin gardner
+%       date: 04/02/15
+%    purpose: get matlab version as a number
+%
+function versionNum = mlrGetMatlabVersion()
+
+% check arguments
+if ~any(nargin == [0])
+  help mlrGetMatlabVersion
+  return
+end
+
+% get version
+v = version;
+
+% parse the string (why, oh, why is it a string anyway?)
+[majorVersion v] = strtok(v,'.');
+[minorVersion v] = strtok(v,'.');
+
+% convert to number
+majorVersion = str2num(majorVersion);
+minorVersion = str2num(minorVersion);
+
+% and return as a single number
+versionNum = majorVersion + minorVersion/10;
+

commit e6268201e0032c6e92bcb4bae7f282e41d8a2399

mrOpenWindow.m: this fixes a bug introduced in commit 96df157 that stopped mrLoadRet remembering the checked status of the mlrAnatomy panel
When load(mrLastView) was replaced by mlrLoadLastView(mrLastView) to fix issues
related to saved figure handles, viewSettings was not output as a field
of the first output of mlrLoadLastView and so settings such as whether panels
should be visible were not applied to the main mrLoadRet window. Fixing
by outputting 2 variables (mrLastView and lastViewSettings) instead of
just one, as now required
by mlrLoadLastView. To streamline the code a bit, I also replaced
mrLastView.view by simply mrLastView since view was the only field left
in the structure

mrLoadRet/View/mrOpenWindow.m
@@ -81,25 +81,25 @@ if ~isempty(which('mlrPlugin')), view = mlrPlugin(view);end
 baseLoaded = 0;
 if ~isempty(mrLastView) && isfile(sprintf('%s.mat',stripext(mrLastView)))
   disppercent(-inf,sprintf('(mrOpenWindow) Loading %s',mrLastView));
-  mrLastView=mlrLoadLastView(mrLastView);
+  [mrLastView, lastViewSettings]=mlrLoadLastView(mrLastView);
   disppercent(inf);
   % if the old one exists, then set up fields
 %   disppercent(-inf,'(mrOpenWindow) Restoring last view');
-  if isfield(mrLastView,'view')
+  if ~isempty(mrLastView)
     % open up base anatomy from last session
-    if isfield(mrLastView.view,'baseVolumes')
+    if isfield(mrLastView,'baseVolumes')
       disppercent(-inf,sprintf('(mrOpenWindow) installing Base Anatomies'));
-      if length(mrLastView.view.baseVolumes) >= 1
+      if length(mrLastView.baseVolumes) >= 1
         baseLoaded = 1;
         % Add it to the list of base volumes and select it
-	for i = 1:length(mrLastView.view.baseVolumes)
+	for i = 1:length(mrLastView.baseVolumes)
 	  % make sure sliceOrientation is not 0
-	  if ~isfield(mrLastView.view.baseVolumes(i),'sliceOrientation') ...
-		|| isequal(mrLastView.view.baseVolumes(i).sliceOrientation,0)
-	    mrLastView.view.baseVolumes(i).sliceOrientation = 1;
+	  if ~isfield(mrLastView.baseVolumes(i),'sliceOrientation') ...
+		|| isequal(mrLastView.baseVolumes(i).sliceOrientation,0)
+	    mrLastView.baseVolumes(i).sliceOrientation = 1;
 	  end
 	  % install the base
-	  view = viewSet(view,'newBase',mrLastView.view.baseVolumes(i));
+	  view = viewSet(view,'newBase',mrLastView.baseVolumes(i));
 	end
       else
         %try to load 
@@ -108,68 +108,68 @@ if ~isempty(mrLastView) && isfile(sprintf('%s.mat',stripext(mrLastView)))
       disppercent(inf);
     end
     % change group
-    view = viewSet(view,'curGroup',mrLastView.view.curGroup);
+    view = viewSet(view,'curGroup',mrLastView.curGroup);
     nScans = viewGet(view,'nScans');
     mlrGuiSet(view,'nScans',nScans);
     if baseLoaded
       % slice orientation from last run
-      view = viewSet(view,'curBase',mrLastView.view.curBase);
-      view = viewSet(view,'sliceOrientation',mrLastView.view.sliceOrientation);
+      view = viewSet(view,'curBase',mrLastView.curBase);
+      view = viewSet(view,'sliceOrientation',mrLastView.sliceOrientation);
       % change scan
-      view = viewSet(view,'curScan',mrLastView.view.curScan);
+      view = viewSet(view,'curScan',mrLastView.curScan);
       % change slice/corticalDepth
-      if viewGet(view,'baseType') && isfield(mrLastView.view.curslice,'corticalDepth')
-        view = viewSet(view,'corticalDepth',mrLastView.view.curslice.corticalDepth);
+      if viewGet(view,'baseType') && isfield(mrLastView.curslice,'corticalDepth')
+        view = viewSet(view,'corticalDepth',mrLastView.curslice.corticalDepth);
       end
-      if isfield(mrLastView.view.curslice,'sliceNum')
-        view = viewSet(view,'curSlice',mrLastView.view.curslice.sliceNum);
+      if isfield(mrLastView.curslice,'sliceNum')
+        view = viewSet(view,'curSlice',mrLastView.curslice.sliceNum);
       end
     end
     % read analyses
-    if isfield(mrLastView.view,'analyses')
-      for anum = 1:length(mrLastView.view.analyses)
-        view = viewSet(view,'newAnalysis',mrLastView.view.analyses{anum});
-%         disppercent(anum /length(mrLastView.view.analyses));
+    if isfield(mrLastView,'analyses')
+      for anum = 1:length(mrLastView.analyses)
+        view = viewSet(view,'newAnalysis',mrLastView.analyses{anum});
+%         disppercent(anum /length(mrLastView.analyses));
       end
-      view = viewSet(view,'curAnalysis',mrLastView.view.curAnalysis);
+      view = viewSet(view,'curAnalysis',mrLastView.curAnalysis);
     end
     % read loaded analyses
-    if isfield(mrLastView.view,'loadedAnalyses')
-      for g = 1:length(mrLastView.view.loadedAnalyses)
-	view = viewSet(view,'loadedAnalyses', mrLastView.view.loadedAnalyses{g},g);
+    if isfield(mrLastView,'loadedAnalyses')
+      for g = 1:length(mrLastView.loadedAnalyses)
+	view = viewSet(view,'loadedAnalyses', mrLastView.loadedAnalyses{g},g);
       end
     end
     % read which scan we were on in for each group
-    if isfield(mrLastView.view,'groupScanNum')
-      for g = 1:length(mrLastView.view.groupScanNum)
-	view = viewSet(view,'groupScanNum', mrLastView.view.groupScanNum(g),g);
+    if isfield(mrLastView,'groupScanNum')
+      for g = 1:length(mrLastView.groupScanNum)
+	view = viewSet(view,'groupScanNum', mrLastView.groupScanNum(g),g);
       end
     end
     
     % read ROIs into current view
-    if isfield(mrLastView.view,'ROIs')
+    if isfield(mrLastView,'ROIs')
       disppercent(-inf,sprintf('(mrOpenWindow) installing ROIs'));
-      for roinum = 1:length(mrLastView.view.ROIs)
-        view = viewSet(view,'newROI',mrLastView.view.ROIs(roinum));
+      for roinum = 1:length(mrLastView.ROIs)
+        view = viewSet(view,'newROI',mrLastView.ROIs(roinum));
       end
-      view = viewSet(view,'currentROI',mrLastView.view.curROI);
-      if ~fieldIsNotDefined(mrLastView.view,'showROIs')
-	view = viewSet(view,'showROIs',mrLastView.view.showROIs);
+      view = viewSet(view,'currentROI',mrLastView.curROI);
+      if ~fieldIsNotDefined(mrLastView,'showROIs')
+	view = viewSet(view,'showROIs',mrLastView.showROIs);
       end
-      if ~fieldIsNotDefined(mrLastView.view,'labelROIs')
-	view = viewSet(view,'labelROIs',mrLastView.view.labelROIs);
+      if ~fieldIsNotDefined(mrLastView,'labelROIs')
+	view = viewSet(view,'labelROIs',mrLastView.labelROIs);
       end
-      if ~fieldIsNotDefined(mrLastView.view,'roiGroup')
-	view = viewSet(view,'roiGroup',mrLastView.view.roiGroup);
+      if ~fieldIsNotDefined(mrLastView,'roiGroup')
+	view = viewSet(view,'roiGroup',mrLastView.roiGroup);
       end
       disppercent(inf);
     end
     % check panels that need to be hidden
-    if isfield(mrLastView,'viewSettings') && isfield(mrLastView.viewSettings,'panels')
-      for iPanel = 1:length(mrLastView.viewSettings.panels)
+    if ~isempty(lastViewSettings) && isfield(lastViewSettings,'panels')
+      for iPanel = 1:length(lastViewSettings.panels)
 	% if it is not displaying then turn it off
-	if ~mrLastView.viewSettings.panels{iPanel}{4}
-	  panelName = mrLastView.viewSettings.panels{iPanel}{1};
+	if ~lastViewSettings.panels{iPanel}{4}
+	  panelName = lastViewSettings.panels{iPanel}{1};
 	  mlrGuiSet(view,'hidePanel',panelName);
 	  % turn off check
 	  mlrAdjustGUI(view,'set',panelName,'Checked','off');